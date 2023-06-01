9d51583320a1ccbd2a3c43d3233425b847b9ed86
rapPayne <bob@creator.net>
2023-06-01 12:54:31 -0500

    Removed server to create a git submodule

--- server/middlewares/logging-middleware.mjs
+++
@@ -1,7 +0,0 @@
-
-export const loggingMiddleware = (req, res, next) => {
-  res.set('X-Powered-By', 'Agile Gadgets');
-  console.log('*'.repeat(80))
-  console.log(`Request: ${req.method} ${req.url}`)
-  next();
-}
