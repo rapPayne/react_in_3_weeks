c8281862c3df745ef6c0a4c65820d7da30a2473a
rapPayne <bob@creator.net>
2023-08-25 08:49:41 -0500

    Made people plural

--- people-directory/src/People.js
+++ people-directory/src/People.js
@@ -4,20 +4,23 @@ export function People() {
   const [people, setPeople] = useState([])
   const [showChangeEmal, setShowChangeEmail] = useState(false);
 
-  const person = people[0] || { name: {}, location: {}, picture: {} }
-  useEffect(() => { fetchPeople(3) }, [])
+  //const person = people[0] || { name: {}, location: {}, picture: {} }
+  useEffect(() => { fetchPeople(5) }, [])
 
   return (
     <>
       <h1>All the cool people</h1>
       {/* <button onClick={() => fetchPeople(10)}>Fetch 10 people</button> */}
-      <section>
-        <h2>{person.name.first} {person.name.last}</h2>
-        <img src={person.picture.large} alt="" />
-        <p>Cell: {person.cell}</p>
-        <p>Email: {person.email}</p>
-        <button onClick={changeEmail}>Change</button>
-        {showChangeEmal && <ChangeEmail email={person.email} />}
+      <section id="peopleSection">
+        {people.map(person => <section key={person.id}>
+          <h2>{person.name.first} {person.name.last}</h2>
+          <img src={person.picture.large} alt="" />
+          <p>Cell: {person.cell}</p>
+          <p>Email: {person.email}</p>
+          <button onClick={changeEmail}>Change</button>
+          {showChangeEmal && <ChangeEmail email={person.email} />}
+        </section>)
+        }
       </section>
     </>
   )
@@ -27,9 +30,11 @@ export function People() {
   }
   async function fetchPeople(numOfPeople) {
     const url = `https://randomuser.me/api/?results=${numOfPeople}`
+    let i = 0;
     const ppl = await fetch(url)
       .then(res => res.json())
-      .then(res => res.results);
+      .then(res => res.results)
+      .then(people => people.map(p => ({ ...p, id: i++, })));
     console.log(ppl);
     setPeople(ppl)
   }
